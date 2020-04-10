27784ead3dadbabcd0acfa90b046469b356a5fd4
MaksimPW <ma.savichev@gmail.com>
2020-04-10 10:49:05 +0700

    set_date for Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  after_initialize :set_date
+
   validates :title, presence: true
   validates :content, presence: true
 
   belongs_to :user
 
   mount_uploader :cover, ImageUploader
+
+  def set_date
+    self.public_date ||= Date.current
+  end
 end
